1da0d7b3dbdc00e61ebd1012919a882ee848d645
James Blades <james.blades@move.com>
2024-10-22 15:37:27 -0600

    Bogus segment API key sample

---
+++ app/src/main/java/com/example/segmenttest/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.segmenttest
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.segmenttest.databinding.ActivityMainBinding
+import com.segment.analytics.kotlin.android.Analytics
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val analytics = Analytics("BOGUS STRING", applicationContext) {
+            // Automatically track Lifecycle events
+            trackApplicationLifecycleEvents = true
+            flushAt = 3
+            flushInterval = 10
+        }
+
+        binding.fab.setOnClickListener { view ->
+            analytics.track("Button Click", buildJsonObject {
+                put("jbladesTest", 42)
+            })
+            analytics.flush()
+            Snackbar.make(view, "Should call button click analytics track", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .setAnchorView(R.id.fab).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
